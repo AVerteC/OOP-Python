cf79eb0823c19a9a7b102584c70641deb603da61
Alazorth <alan.r.chen@gmail.com>
2018-06-20 11:31:26 -0700

    save before lunch

---
+++ OOP Part 2/ex00/linkbuilder.py
@@ -0,0 +1,12 @@
+from singlylist_class import SinglyList
+from node_class import Node
+
+
+class LinkBuilder():
+    def __init__(self):
+        print("creating head of list")
+        sl = SinglyList()
+
+        for x in range(0, 10):
+            print("creating node " + str(x))
+            sl.add_head(Node(x))
---
+++ OOP Part 2/ex00/print_list.py
@@ -0,0 +1,15 @@
+from linkbuilder import LinkBuilder
+
+
+class LiList:
+
+    if __name__ == "__main__":
+        thislist = LinkBuilder()
+        print_list()
+
+    def print_list(self, list_head):
+        print("HeadValue: ")
+        print(thislist.head)
+        print("NodeValue: ")
+        print(thislist.content)
+
