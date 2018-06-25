6ea3c3b9f993b513b2e31ff68324b2e642fdbd5b
Alazorth <alan.r.chen@gmail.com>
2018-06-25 15:48:34 -0700

    leave 42

--- OOP Part 3/math.py
+++ OOP Part 3/math.py
@@ -6,11 +6,17 @@ if __name__ == "__main__":
 
     # separate numbers
     data = x.split(",")
-
+    sum = 0
     # add numbers to stack and remove whitespace
     for x in range(0, len(data)):
         data[x] = str(data[x]).replace(" ", "")
         mst.push(float(data[x]))
+    for x in range(0, mst.size):
+        
+        product *= data[x]
     print(data)
-    print(mst)
+    print("Total count = " + str(mst.size))
+    print("Sum = " + str(sum))
+    print("Product = " + str(product))
+
 
