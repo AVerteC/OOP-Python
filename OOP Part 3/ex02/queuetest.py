ca6c78a693ee9fa06367077cd3c393eece227b39
Alazorth <alan.r.chen@gmail.com>
2018-06-27 17:04:17 -0700

    leave 42 on wed.

---
+++ OOP Part 3/ex02/queuetest.py
@@ -0,0 +1,22 @@
+from queue import Queue
+
+if __name__ == "__main__":
+
+    q = Queue()
+    for x in range(4):
+        print("adding queue item: " + str(x))
+        # enqueue test
+        q.enqueue(x)
+
+    # size test
+    print("size: " + str(q._size))
+
+    # emptiness test
+    print("empty? " + str(q.isEmpty))
+    print(q)
+    print("front " + str(q.front))
+    #print(q.front.next)
+    print("back " + str(q.back))
+    # dqueue test
+    q.dequeue()
+    print(q)
